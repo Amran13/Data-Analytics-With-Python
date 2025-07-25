35b199af72fb4bee149fcdd21bdeaf0b2e5008b2
amranhossain <amranhossainchy@gmail.com>
2025-07-25 09:30:51 +0600

    student marks added

--- Matplotlib/student_mark_comparison.py
+++ Matplotlib/student_mark_comparison.py
@@ -9,6 +9,6 @@ plt.bar(students, marks, color='royalblue')
 plt.title("Mark Comparison Chart")
 plt.xlabel('Name of the students')
 plt.ylabel('Marks')
-plt.grid()
-
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.savefig('Charts/student-marks.png')
 plt.show()
